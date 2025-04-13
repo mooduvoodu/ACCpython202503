b49cb4caf2fd15887a9b35b01d04719d9de7ead5
Jafar Dehghan <76612521+JAYDI2020@users.noreply.github.com>
2025-04-12 22:39:16 -0500

    Add files via upload

---
+++ Jafar/ex1.py
@@ -0,0 +1,7 @@
+# messy code
+AMT= 100
+def calc_tax(amount):
+    rate = 0.1
+    tax = amount * rate
+    print("Tax:", tax)
+calc_tax(AMT)
---
+++ Jafar/ex2.py
@@ -0,0 +1,5 @@
+months = 12
+temperature = 36.6
+message = "Hello"
+colors = ['red', 'blue', 'green']
+book = {"title": "book1", "auther": "auther1", "year": 2023}
