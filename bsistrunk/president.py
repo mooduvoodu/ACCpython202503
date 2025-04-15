43f5cc62728d17c53cee0dc203c3525596c84e9a
Blake Sistrunk <112571550+BSistrunk@users.noreply.github.com>
2025-04-15 00:04:18 +0000

    Week 1 Homework

---
+++ bsistrunk/president.py
@@ -0,0 +1,4 @@
+class Prez:
+    def veto(self,billname):
+        print("no way that " + billname + "sucks")
+    name = 'placeholder name'
---
+++ bsistrunk/test2.py
@@ -0,0 +1,16 @@
+import math
+from president import Prez
+
+
+print('hello')
+
+
+mynewprez = Prez()
+mynewprez2 = Prez()
+
+mynewprez.name = "Trump"
+mystring = 'somebill509'
+mynewprez.veto(mystring)
+
+
+print(mynewprez.name)
