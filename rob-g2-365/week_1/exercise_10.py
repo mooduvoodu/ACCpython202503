955e9ebfb0b553bf01883358a8453008e2a05bc7
Rob Gorsegner <rob-g2-365@gmail.com>
2025-04-17 11:06:46 -0500

    Fixed week1 exercise 10

--- rob-g2-365/week_1/exercise_10.py
+++ rob-g2-365/week_1/exercise_10.py
@@ -3,8 +3,9 @@
 # (int or float) and the return type is a number. Then, call your function with a couple of examples 
 # (e.g., square(5), square(2.5)) and print the results.
 # Bonus: Inside the function, add a check to ensure the argument is an int or float, and print a friendly error message if not.
+from typing import Union
 
-def square(number: float | int) ->  float | int:
+def square(number: Union[float, int]) ->  Union[float, int]:
   return number * number
 
 print("square(5) :", square(5))
